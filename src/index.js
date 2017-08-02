da446faebe3d6a34f809382dd611f3ebdb93c57e
David Villavicencio <david@hologram.io>
2017-08-02 13:51:25 -0500

    Migrate from the deprecated React.propTypes to prop-types package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * @param  {string} options.form     A unique key to identify your form throughout the app
